e3c6bfa5f45c15051aa9b5befbbabb6f925026bf
r4vemode <62845988+r4vemode@users.noreply.github.com>
2022-03-03 12:29:14 +0300

    Update reflex.java

--- reflex.java
+++ reflex.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main { 
     public static void main(String[] args) throws Exception {
             SomeClass someClass = new SomeClass();
             Class clss = someClass.getClass();
